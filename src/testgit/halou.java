4fdc9f5bce39fbbb342722538b4016c57bb0e3a8
赵潞松 <@windows10.microdone.cn>
2019-08-29 22:39:06 +0800

    ss

--- src/testgit/halou.java
+++ src/testgit/halou.java
@@ -5,7 +5,7 @@ public class halou {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Hello  Git");
-		System.out.println("我在提交a");
+		System.out.println("我在提交");
 	}
 
 }
